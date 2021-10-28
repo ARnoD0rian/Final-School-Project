28d04a3aa740c23a78ca2f422d97733612f3a7ba
Arno <92749018+ARnoD0rian@users.noreply.github.com>
2021-10-28 22:51:09 +0300

    Create logo.php

---
+++ logo.php
@@ -0,0 +1,19 @@
+<?php
+$email = filter_var(trim($_POST['email']),FILTER_SANITIZE_STRING);
+$pass = filter_var(trim($_POST['pass']),FILTER_SANITIZE_STRING);
+require "sql.php";
+$result=$mysql->query("SELECT * FROM `register` WHERE `email`='$email' AND `pass`='$pass'") ;
+$user=$result->fetch_assoc();
+if (count ($user)==0) { 
+setcookie("error3",12345, time()+3600,"/");
+$mysql->close();
+header('location: log.php' );
+  } else {
+    setcookie("name", $user['name'], time()+3600*24*30,"/");
+    setcookie("patryonic", $user['patryonic'], time()+3600*24*30,"/");
+    setcookie("class", $user['class'], time()+3600*24*30,"/");
+    setcookie("error3",12345, time()-3600,"/");
+$mysql->close();
+header('location: lich.php' ); 
+  } 
+?>
